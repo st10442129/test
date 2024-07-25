1a2410b4e179d9043b9da0dd63a9d636f90d545f
unknown <st10442129@vcconnect.edu.za>
2024-07-25 12:19:53 +0300

    test

---
+++ src/main/java/com/mycompany/loops/Loops.java
@@ -0,0 +1,18 @@
+
+
+public class Loops {
+
+    public static void main(String[] args) {
+        
+        int x;
+        
+        System.out.println(x);
+        
+        x = 0;
+        
+        while(x < 10)
+        {
+            
+        }      
+    }
+}
---
+++ src/test/java/com/mycompany/loops/LoopsTest.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.loops;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class LoopsTest {
+    
+    public LoopsTest() {
+    }
+
+    /**
+     * Test of main method, of class Loops.
+     */
+    @org.junit.jupiter.api.Test
+    public void testMain() {
+    }
+    
+}
